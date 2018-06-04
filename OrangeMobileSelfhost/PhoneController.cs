4ddf5af0654128537754a924b3894ce5e969e940
Nirmal <w.v.nirmalfernando@gmail.com>
2018-06-04 23:55:00 +1200

    First Commit

---
+++ OrangeMobileSelfhost/PhoneController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace OrangeMobileSelfhost
+{
+    public class PhoneController: ApiController
+    {
+        public List<string> GetPhoneList() {
+            DataTable lcResult = ClsDBConnection.GetDataTable("SELECT name FROM tbl_all_products", null);
+            List<string> lcNames = new List<string>();
+            foreach (DataRow dr in lcResult.Rows)
+                lcNames.Add((string)dr[0]); return lcNames;
+        }
+    }
+}
